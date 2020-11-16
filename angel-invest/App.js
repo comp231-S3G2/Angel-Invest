3cd588f8849ef8de1363ef94d1b91571e63f36d3
Shiu Tung (James) Chan <schan190@my.centennialcollege.ca>
2020-11-16 01:05:02 -0500

    added hello world

--- angel-invest/App.js
+++ angel-invest/App.js
@@ -6,7 +6,16 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <HelloWorld/>
       <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const HelloWorld = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
     </View>
   );
 }
