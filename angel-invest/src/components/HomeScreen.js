08012fe6ed649a1bfa4eb581994072f8e277064a
Yashwanth Govula <ygovula@my.centennialcollege.ca>
2020-11-23 14:16:58 -0600

    changes

--- angel-invest/src/components/HomeScreen.js
+++ angel-invest/src/components/HomeScreen.js
@@ -1,16 +1,28 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { View, Text, Button, StyleSheet, StatusBar } from 'react-native';
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+
+const HomeScreen = ({navigation}) => {
+
+  
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle= { theme.dark ? "light-content" : "dark-content" }/>
+        <Text style={{color: colors.text}}>Home Screen</Text>
       <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        title="Go to details screen"
+        onPress={() => navigation.navigate("Details")}
       />
-    </View>
-  );
-}
+      </View>
+    );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+});
--- angel-invest/src/components/PostScreen.js
+++ angel-invest/src/components/PostScreen.js
@@ -1,16 +1,32 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-function PostScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
+const DetailsScreen = ({navigation}) => {
+    return (
+      <View style={styles.container}>
+        <Text>Details Screen</Text>
+        <Button
+            title="Go to details screen...again"
+            onPress={() => navigation.push("Details")}
+        />
+        <Button
+            title="Go to home"
+            onPress={() => navigation.navigate("Home")}
+        />
+        <Button
+            title="Go back"
+            onPress={() => navigation.goBack()}
+        />
+      </View>
+    );
+};
+
+export default DetailsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+});
