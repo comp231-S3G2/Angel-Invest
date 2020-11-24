800e561419728b3392bc71f282956e270357639b
Yashwanth Govula <ygovula@my.centennialcollege.ca>
2020-11-24 01:15:03 -0600

    multiple files selection

--- angel-invest/src/components/PostScreen.js
+++ angel-invest/src/components/PostScreen.js
@@ -8,20 +8,19 @@ import DocumentPicker from 'react-native-document-picker';
 const PostScreen = ({navigation}) => {
 
 
-  const [value, onChangeText] = React.useState('Title');
-
-   openDocumentFile()
-  {
+  const openDocumentFile =async() =>{
     try {
-      const res = await DocumentPicker.pick({
+      const results = await DocumentPicker.pickMultiple({
         type: [DocumentPicker.types.images],
       });
-      console.log(
-        res.uri,
-        res.type, // mime type
-        res.name,
-        res.size
-      );
+      for (const res of results) {
+        console.log(
+          res.uri,
+          res.type, // mime type
+          res.name,
+          res.size
+        );
+      }
     } catch (err) {
       if (DocumentPicker.isCancel(err)) {
         // User cancelled the picker, exit any dialogs or menus and move on
