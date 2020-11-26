66cdbd2655cf99a3e126cb1ff5155bdd18465aeb
Yashwanth Govula <ygovula@my.centennialcollege.ca>
2020-11-25 22:12:53 -0600

    documet picker added

--- angel-invest/src/navigations/CreatePosts/view.tsx
+++ angel-invest/src/navigations/CreatePosts/view.tsx
@@ -3,14 +3,35 @@ import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
 import Style from './style';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import DropDownPicker from 'react-native-dropdown-picker';
-import * as ImagePicker from 'expo-image-picker';
-import * as DocumentPicker from 'expo-document-picker';
+import DocumentPicker from 'react-native-document-picker';
 import Constants from 'expo-constants';
 
 
 
 const PostScreen = () => {
 
+
+  const openDocumentFile =async() =>{
+    try {
+      const results = await DocumentPicker.pickMultiple({
+        type: [DocumentPicker.types.allFiles],
+      });
+      for (const res of results) {
+        console.log(
+          res.uri,
+          res.type, // mime type
+          res.name,
+          res.size
+        );
+      }
+    } catch (err) {
+      if (DocumentPicker.isCancel(err)) {
+        // User cancelled the picker, exit any dialogs or menus and move on
+      } else {
+        throw err;
+      }
+    }
+  }
 
     return (
       <View style={Style.container}>
@@ -50,7 +71,10 @@ const PostScreen = () => {
       placeholder ="Equity Goal" underlineColorAndroid={'transparent'}
       
        />
-  
+  <TouchableOpacity onPress={() =>openDocumentFile} style={{borderRadius:10, padding:10,width:500,height:50, alignItems: "center", backgroundColor:"#59cbbd"}}>
+        <Text style = {Style.appButtonText}>Add Documents</Text>
+        </TouchableOpacity>
+
        
        <TouchableOpacity style={Style.Button}>
         <Text style = {Style.appButtonText}>Add Post</Text>
