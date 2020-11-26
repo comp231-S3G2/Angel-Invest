58b1bf7c3d723fd7b712c185481e325ced73a57c
Yashwanth Govula <ygovula@my.centennialcollege.ca>
2020-11-25 21:30:22 -0600

    create post styles added

---
+++ angel-invest/src/navigations/CreatePosts/style.ts
@@ -0,0 +1,31 @@
+import {StyleSheet} from 'react-native';
+
+
+export default StyleSheet.create({
+    container: {
+      flex: 1, 
+      alignItems: 'center', 
+      justifyContent: 'center'
+    },
+    textinput:{
+      flexDirection:"row",
+      paddingHorizontal:10,
+      marginHorizontal:55,
+      borderWidth:2,
+      marginTop:100,
+      alignSelf: 'stretch',
+      height: 40,
+      marginBottom: 30,
+      color:'#000000',
+      borderBottomColor:'#A9A9A9',
+      borderBottomWidth: 1,
+    },
+    Button: {
+      alignSelf: 'stretch',
+      alignItems: 'center',
+      padding:20,
+      backgroundColor: '#59cbbd',
+      marginTop: 30,
+      
+    }
+  });
